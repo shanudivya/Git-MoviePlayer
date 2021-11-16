42184b530eb4addd9391d780c39b80fc228bcdf9
unknown <shanu.divya123@gmail.com>
2021-11-16 12:54:09 +0530

    updated code

--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,6 +1,27 @@
 import React from "react";
+import { useParams } from "react-router-dom";
+//config
+import { Image_BASE_URL, POSTER_SIZE } from '../config';
+//component
+import Grid from './Grid';
+import Spinner from './Spinner';
 
-const Movie = () =>
+//Hook
+import { useMovieFetch } from "../hooks/useMovieFetch";
+//Image
+import NoImage from '../images/no_image.jpg';
+
+const Movie = () => {
+    const {movieId} = useParams();
+
+const {state: movie, loading, error}=useMovieFetch(movieId)
+
+    return (
+        <>
+            <div>Movie</div>
+        </>
+    )
+}
 <div>Movie</div>
 
 export default Movie;
